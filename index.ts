a71121c2fcacb900b16b49bc6922eb4adb87f190
TheMysterys <the-mystery@outlook.com>
2023-03-22 16:44:33 +1300

    init commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Hello World and Welcome to TypeScript\n");
+
+const myName: String = "Alex";
+
+console.log("My name is " + myName + "\n");
+
+console.log("The following is a for loop that counts from 1 to 10\n");
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1);
+}
