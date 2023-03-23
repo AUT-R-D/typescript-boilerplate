6072b25dad40b518cfc5b380f632941eb0fbfa0b
Carl1131 <carlsantos31@yahoo.com>
2023-03-23 15:36:12 +1300

    Simple Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+
+
 console.log("Hello World and Welcome to TypeScript\n");
 
 const myName: String = "Alex";
@@ -5,6 +7,8 @@ const myName: String = "Alex";
 console.log("My name is " + myName + "\n");
 
 console.log("The following is a for loop that counts from 1 to 10\n");
+
 for (let i = 0; i < 10; i++) {
   console.log(i + 1);
 }
+
